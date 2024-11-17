009f6d27cd7bf9331a533e36995f9ee5c944211e
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-11-17 17:37:24 -0300

    feat: base button

---
+++ src/view/components/Button.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface ButtonProps extends ComponentProps<"button"> {}
+
+export function Button(props: ButtonProps) {
+    return (
+        <button
+            {...props}
+            className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100
+            px-6 h-12 rounded-2xl font-medium text-white disabled:text-gray-400
+            disabled:cursor-not-allowed transition-all"
+        />
+    );
+}
