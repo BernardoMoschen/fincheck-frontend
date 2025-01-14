0803f3687c0e0602e829aff2e3fc6a19b835caa7
BernardoMoschen <bernardomoschen.dev@gmail.com>
2025-01-14 15:13:13 -0300

    feat: spinner component

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,5 +1,6 @@
 import { ComponentProps } from "react";
 import { cn } from "../../app/utils/cn";
+import { Spinner } from "./Spinner";
 
 
 interface ButtonProps extends ComponentProps<"button"> {
@@ -20,6 +21,8 @@ export function Button({
                 "bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl font-medium text-white disabled:text-gray-400 disabled:cursor-not-allowed transition-all",
                 className
             )}
-        />
+        >
+            <Spinner />
+        </button>
     );
 }
