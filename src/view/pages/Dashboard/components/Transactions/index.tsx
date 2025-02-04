df31d5aa7fa83c829b490e0d356bb9a0291284f5
BernardoMoschen <bernardomoschen.dev@gmail.com>
2025-02-03 21:43:40 -0300

    wip: transcations cards

--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -5,11 +5,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { MONTHS } from "../../../../../app/config/constants";
 import { SliderOption } from "./SliderOption";
 import { SliderNavigation } from "./SliderNavigation";
+import { formatCurrency } from "../../../../../app/utils";
+import { CategoryIcon } from "../../../../icons/categories/CategoryIcon";
 
 export function Transactions() {
     return (
-        <div className="bg-gray-100 rounded-2xl w-full h-full md:p-10 px-4 py-8">
-            <header >
+        <div className="bg-gray-100 rounded-2xl w-full h-full p-10 flex flex-col">
+            <header>
                 <div className="flex items-center justify-between">
                     <button className="flex items-center gap-2">
                         <TransactionsIcon />
@@ -34,9 +36,33 @@ export function Transactions() {
                     </Swiper>
                 </div>
             </header>
-            <div className="">
-                Conteúdo
+            <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
+                <div className="rounded-2xl bg-white p-4 flex items-center justify-between gap-4">
+                    <div className="flex-1 flex items-center gap-3">
+                        <CategoryIcon type="expense" />
+                        <div>
+                            <strong className="font-bold tracking-[-0.5px]">Almoço</strong>
+                            <span className="text-sm text-gray-600 block">04/04/04</span>
+                        </div>
+                    </div>
+                    <span className="text-red-800 tracking-[-0.5px] font-medium">
+                        {formatCurrency(1230)}
+                    </span>
+                </div>
+                <div className="rounded-2xl bg-white p-4 flex items-center justify-between gap-4">
+                    <div className="flex-1 flex items-center gap-3">
+                        <CategoryIcon type="income" />
+                        <div>
+                            <strong className="font-bold tracking-[-0.5px]">Almoço</strong>
+                            <span className="text-sm text-gray-600 block">04/04/04</span>
+                        </div>
+                    </div>
+                    <span className="text-green-800 tracking-[-0.5px] font-medium">
+                        {formatCurrency(1230)}
+                    </span>
+                </div>
             </div>
+
         </div >
     );
 }
--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -9,7 +9,7 @@ export default function Dashboard() {
                 <Logo className="h-6 text-teal-900" />
                 <UserMenu />
             </header>
-            <main className="flex-1 flex flex-col md:flex-row gap-4">
+            <main className="flex-1 flex flex-col md:flex-row gap-4 max-h-full">
                 <div className="w-full md:w-1/2">
                     <Accounts />
                 </div>
