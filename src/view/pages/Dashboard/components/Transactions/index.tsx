68aeeab9ac5be51bd7ce50a719887635d399939d
BernardoMoschen <bernardomoschen.dev@gmail.com>
2025-02-05 15:59:11 -0300

    wip: loading state mockups

--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -12,16 +12,17 @@ import { Spinner } from "../../../../components";
 import emptyStateImage from '../../../../../assets/empty-state.svg'
 
 export function Transactions() {
-    const { transactions, areValuesVisible, isLoading } = useTransactionsController()
+    const { transactions, areValuesVisible, isInitialLoading, isReFetching } = useTransactionsController()
+    const hasTransactions = !!transactions.length
 
     return (
         <div className="bg-gray-100 rounded-2xl w-full h-full p-10 flex flex-col">
-            {isLoading && (
+            {isInitialLoading && (
                 <div className="h-full flex items-center justify-center">
                     <Spinner className="text-white fill-teal-950/50 w-10 h-10" />
                 </div>
             )}
-            {!isLoading && (
+            {!isInitialLoading && (
                 <>
                     <header>
                         <div className="flex items-center justify-between">
@@ -48,40 +49,49 @@ export function Transactions() {
                             </Swiper>
                         </div>
                     </header>
-                    {!transactions.length && (
-                        <div className="flex flex-col items-center justify-center h-full ">
-                            <img src={emptyStateImage} alt="Empty State" />
-                            <p className="text-gray-700">Não encontramos nenhuma transação</p>
-                        </div>
-                    )}
-                    {!!transactions.length && (
-                        <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
-                            <div className="rounded-2xl bg-white p-4 flex items-center justify-between gap-4">
-                                <div className="flex-1 flex items-center gap-3">
-                                    <CategoryIcon type="expense" />
-                                    <div>
-                                        <strong className="font-bold tracking-[-0.5px]">Almoço</strong>
-                                        <span className="text-sm text-gray-600 block">04/04/04</span>
-                                    </div>
-                                </div>
-                                <span className={cn("text-red-800 tracking-[-0.5px] font-medium", !areValuesVisible && 'blur-sm')}>
-                                    {formatCurrency(1230)}
-                                </span>
+                    <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
+                        {(!hasTransactions || !isReFetching) && (
+                            <div className="flex flex-col items-center justify-center h-full ">
+                                {isReFetching && (
+                                    <Spinner />
+                                )}
+                                {!hasTransactions && (
+                                    <>
+                                        <img src={emptyStateImage} alt="Empty State" />
+                                        <p className="text-gray-700">Não encontramos nenhuma transação!</p>
+                                    </>
+                                )}
                             </div>
-                            <div className="rounded-2xl bg-white p-4 flex items-center justify-between gap-4">
-                                <div className="flex-1 flex items-center gap-3">
-                                    <CategoryIcon type="income" />
-                                    <div>
-                                        <strong className="font-bold tracking-[-0.5px]">Almoço</strong>
-                                        <span className="text-sm text-gray-600 block">04/04/04</span>
+                        )}
+                        {(hasTransactions && !isReFetching) && (
+                            <>
+                                <div className="rounded-2xl bg-white p-4 flex items-center justify-between gap-4">
+                                    <div className="flex-1 flex items-center gap-3">
+                                        <CategoryIcon type="expense" />
+                                        <div>
+                                            <strong className="font-bold tracking-[-0.5px]">Almoço</strong>
+                                            <span className="text-sm text-gray-600 block">04/04/04</span>
+                                        </div>
                                     </div>
+                                    <span className={cn("text-red-800 tracking-[-0.5px] font-medium", !areValuesVisible && 'blur-sm')}>
+                                        {formatCurrency(1230)}
+                                    </span>
                                 </div>
-                                <span className={cn("text-green-800 tracking-[-0.5px] font-medium", !areValuesVisible && 'blur-sm')}>
-                                    {formatCurrency(1230)}
-                                </span>
-                            </div>
-                        </div>
-                    )}
+                                <div className="rounded-2xl bg-white p-4 flex items-center justify-between gap-4">
+                                    <div className="flex-1 flex items-center gap-3">
+                                        <CategoryIcon type="income" />
+                                        <div>
+                                            <strong className="font-bold tracking-[-0.5px]">Almoço</strong>
+                                            <span className="text-sm text-gray-600 block">04/04/04</span>
+                                        </div>
+                                    </div>
+                                    <span className={cn("text-green-800 tracking-[-0.5px] font-medium", !areValuesVisible && 'blur-sm')}>
+                                        {formatCurrency(1230)}
+                                    </span>
+                                </div>
+                            </>
+                        )}
+                    </div>
                 </>)}
         </div >
     );
